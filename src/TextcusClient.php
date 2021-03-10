5d510940d34907d9c9258bc6ad263c96b0517e02
Abdul Kudus <abdulkudus2922@gmail.com>
2021-03-10 18:07:38 +0000

    added dlr and type parameters

--- src/TextcusClient.php
+++ src/TextcusClient.php
@@ -15,7 +15,11 @@ class TextcusClient
 
         $apiKey = config('services.textcus.api_key');
 
-        $response = $client->get("api/send?apikey={$apiKey}&destination={$to}&source={$from}&message={$content}");
+        $dlr = config('services.textcus.dlr');
+
+        $type = config('services.textcus.type');
+
+        $response = $client->get("api/send?apikey={$apiKey}&destination={$to}&source={$from}&dlr={$dlr}&type={$type}&message={$content}");
 
         $response = json_decode($response->getBody()->getContents());
 
